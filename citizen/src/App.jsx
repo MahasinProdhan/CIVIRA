0b0502b106ee957508b1075d1b60f28df217986a
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2025-12-12 22:52:01 +0530

    changed the navbar

--- citizen/src/App.jsx
+++ citizen/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-red-400">This is main app</div>;
+  return (
+    <div className="bg-red-400">
+      <ul className="hidden md:flex gap-8 text-gray-700 font-medium">
+        <li className="hover:text-primaryColor cursor-pointer">Home</li>
+        <li className="hover:text-primaryColor cursor-pointer">Dashboard</li>
+        <li className="hover:text-primaryColor cursor-pointer">Register</li>
+        <li className="hover:text-primaryColor cursor-pointer">Login</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
